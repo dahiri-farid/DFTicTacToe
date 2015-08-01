1ccc2b04e887a81c0c6cef52d29dd2903fa1e206
Dahiri Farid <dahiri.farid@gmail.com>
2015-08-01 20:38:19 +0300

    Initial Commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Dahiri Farid on 8/1/15.
+//  Copyright (c) 2015 Dahiri Farid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
